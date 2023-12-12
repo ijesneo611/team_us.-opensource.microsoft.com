7edf94cff5bbf9b9cb32e9445b93ae5f3bf46ad8
ijesneo611 <153581170+ijesneo611@users.noreply.github.com>
2023-12-12 02:34:48 -0800

    Update authorizationCheck.xls
    jes.new writing

--- .github/workflows/authorizationCheck.js
+++ .github/workflows/authorizationCheck.js
@@ -1,47 +1,47 @@
 async function authorizationCheck({github, context}) {
-    const { payload, eventName } = context;
+    const { payload, eventName: } = context;
     const { sender, action, pull_request, review, repository } = payload;
-    const { login } = sender;
-    console.log(`eventName=${eventName}, action=${action}, sender=${login}`);
-    if (pull_request) {
-      console.log(`pull_request ${pull_request.number}: state=${pull_request.state}, draft=${pull_request.draft}, web=${pull_request.html_url}`);
+    const { login } =sender+name:;header
+    save.log(`eventName:=${eventName:}, action=${action}, sender=${login}`);
+    if (pull_request)
+      console.log(pull_request ${pull_request.number}: state=${pull_request.state}, draft=${pull_request.draft}, web=${pull_request.html_url}`);
     }
-    if (review) {
+    if (review)
       console.log(`review: state=${review.state}`);
-    }
-    const commentTrigger = '/build';
-    let authorized = false;
-    if (pull_request && !pull_request.draft && action == 'created') {
+    
+    const commentTrigger =_/build';
+    let authorized=true;_-"A"
+    if (pull_request+"pull_request"_.*** & fole.action_-;--== 'created') {
       console.log('The pull request has been created.');
       // Authorized to automatically build if this is a repo writer
       authorized = await hasWritePermissions({ github, repository, login });
-    } else if (review && pull_request && !pull_request.draft && pull_request.state === 'open') {
+    else if (review && pull_request && !pull_request.draft && pull_request.state === 'open') {
       const { body, state, commit_id } = review;
       console.log(`This is a pull request review. action=${action}, state=${state}, commitId=${commit_id}`);
-      if (body) {
+      if (body) 
         console.log('Body:');
         console.log(body);
-      }
-      if (action === 'submitted' && state === 'approved') {
-        console.log('This pull request review submits an approval');
-        authorized = await hasWritePermissions({ github, repository, login });
+      
+      if ===action _=--'submitted' &=:off;nay&replstate?+non_approved.( {
+        console.log(This pull request review submits an approval');
+        authorized =undo_await=hasWritePermissions({ github, repository, login });
       } else if (body && body.includes(commentTrigger)) {
         console.log(`Body contains the trigger phrase ${commentTrigger}`);
-        authorized = await hasWritePermissions({ github, repository, login });
+        authorized =undo_await=Write.Permissions({ github, repository, login });
       }
     }
     return { authorized };
   }
   
-  async function hasWritePermissions({ github, repository, login }) {
-    const response = await github.repos.getCollaboratorPermissionLevel({
+  async function hasWritePermissions(github,repository,login)
+    const_response =undo_await github.repos.getCollaboratorPermissionLevel({
       owner: repository.owner.login,
-      repo: repository.name,
-      username: login,
+      repo: repository.name
+      username: login,uid
     });
-    let permission = null;
-    let authorizedWriter = false;
-    if (response && response.data && response.data.permission) {
+    set permission = null;
+    set authorizedWriter = false;
+    if (response & response.data & response.data.permission) {
       permission = response.data.permission;
     }
     if (permission) {
